a61d40c98890d09160217470c20aa88079a89671
Angel30-G <83377658+Angel30-G@users.noreply.github.com>
2022-06-14 18:44:08 -0600

    Se agregan las librerias del Api

--- app/src/main/java/Buscador_De_Imagenes/conectarBaseDeDatos.java
+++ app/src/main/java/Buscador_De_Imagenes/conectarBaseDeDatos.java
@@ -1,11 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Buscador_De_Imagenes;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
 /**
  *
  * @author gabri
@@ -15,6 +16,7 @@ public class conectarBaseDeDatos {
         FileInputStream serviceAccount
            = new FileInputStream("path/to/serviceAccountKey.json");
 
+        
         FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .setDatabaseUrl("https://buscadorimagenespoo-default-rtdb.firebaseio.com")
@@ -23,3 +25,4 @@ public class conectarBaseDeDatos {
         FirebaseApp.initializeApp(options);
     }
 }
+
